99e32d59feb150ca57fe92ad281ef24e0c1a5ebc
Aryan Agarwal <aryankush025@gmail.com>
2024-05-07 21:59:24 +0530

    chore: Update Firebase configuration for easy access during review

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -6,14 +6,15 @@ import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
+// My web app's Firebase configuration
+// I have to commit this for easy access to the Firebase configuration for the reviewer
 const firebaseConfig = {
-  apiKey: process.env.apiKey,
-  authDomain: process.env.authDomain,
-  projectId: process.env.projectId,
-  storageBucket: process.env.storageBucket,
-  messagingSenderId: process.env.messagingSenderId,
-  appId: process.env.appId,
+  apiKey: "AIzaSyCnU4jy3YSk_O-WlWqqKdXkzLDhWhCiD0s",
+  authDomain: "todo-next14-33ebf.firebaseapp.com",
+  projectId: "todo-next14-33ebf",
+  storageBucket: "todo-next14-33ebf.appspot.com",
+  messagingSenderId: "359018630123",
+  appId: "1:359018630123:web:2037833e0e2a39322b67ae",
 };
 
 // Initialize Firebase
